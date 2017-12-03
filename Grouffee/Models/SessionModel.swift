d6503ce525ada3430cb9e8186c160baca3d98692
Unknown <ravian.hartono@gmail.com>
2017-12-03 23:03:25 +0700

    Add Session Model

---
+++ Grouffee/Models/SessionModel.swift
@@ -0,0 +1,17 @@
+//
+//  SessionModel.swift
+//  Grouffee
+//
+//  Created by Christhopher Ravian Hartono on 03/12/17.
+//  Copyright © 2017 Communication iOS Foundation Batch 4. All rights reserved.
+//
+
+import UIKit
+
+struct GrouffeeSession
+{
+    var user : User
+    var startTime : Date
+    var endTime : Date?
+    
+}
